e22b0c0df01c6cbb2e12abfbed8ed263721789e7
Trasiu <kuba6356@gmail.com>
2022-11-24 12:34:43 +0100

    Added some basic UI

--- javascript.js
+++ javascript.js
@@ -1,83 +1,135 @@
 console.log("do ilu gramy?");
-let rundy = prompt("gramy do:");
-console.log("ok gramy do "+rundy+" rund")
+let maxScore = prompt("gramy do:");
+if(maxScore < 1){
+    console.log("ok to nie gramy");
+}
+else if(maxScore == 1)
+    console.log("ok gramy do 1 punkta");
+else{
+    console.log("ok gramy do "+maxScore+" punktów");
+}
+
+
+let scoreGracz = 0;
+let scoreKomputer = 0;
+const Body = document.querySelector("#Body");
+const Wygrał = document.createElement("div")
+
+const Fire = document.createElement("button")
+Fire.setAttribute("id","Fire")
+Fire.textContent = "Fire"  
+Body.appendChild(Fire)
+
+const Water = document.createElement("button")
+Water.setAttribute("id","Water")
+Water.textContent = "Water"  
+Body.appendChild(Water)
+
+const Grass = document.createElement("button")
+Grass.setAttribute("id","Grass")
+Grass.textContent = "Grass"  
+Body.appendChild(Grass)
+
+const Punkty = document.createElement("div")
+Punkty.setAttribute("id","Punkty")
+Punkty.textContent = "KOMPUTER vs GRACZ";
+Body.appendChild(Punkty)
+
+const Komputer = document.createElement("div")
+Komputer.setAttribute("id","Komputer")
+Komputer.textContent = scoreKomputer;
+Body.appendChild(Komputer)
+
+const Gracz = document.createElement("div")
+Gracz.setAttribute("id","Gracz")
+Gracz.textContent = scoreGracz;
+Body.appendChild(Gracz)
 
 function KomputerRuch(){
-    let ranInt =  Math.floor(Math.random() * 3) + 1
-    if(ranInt == 1){
-        return "KAMIEŃ"
+    let randomInt =  Math.floor(Math.random() * 3) + 1;
+    if(randomInt == 1){
+        return "FIRE";
     }
-    else if(ranInt == 2){
-        return "NOŻYCE"
+    else if(randomInt == 2){
+        return "GRASS";
     }
     else
-        return "PAPIER"
-}
+        return "WATER";
+} 
 
-let array = ["KAMIEŃ","PAPIER","NOŻYCE"]
-
-function GraczRuch(){
-    let gracz = prompt("wybierz kamień, papier lub nożyce")
-    gracz = gracz.toUpperCase()
-    while(!array.includes(gracz)){
-        gracz = prompt("Ej ale to musi być KAMIEŃ, PAPIER albo NOŻYCE!!!")
-        gracz = gracz.toUpperCase()
-    }
-    return gracz
-}
-
-function gra(RuchGracz, RuchKomputer){
-    console.log("gracz: " + RuchGracz)
-    console.log("komputer: " + RuchKomputer)
-        if(RuchGracz == "KAMIEŃ"){
-            if(RuchKomputer == "PAPIER"){
-                return "nie";
-            }
-            else if(RuchKomputer == "NOŻYCE"){
-                return "tak";
-            }}
-        if(RuchGracz == "PAPIER"){
-            if(RuchKomputer == "NOŻYCE"){
-                return "nie"
-            }
-            else if(RuchKomputer == "KAMIEŃ"){
-                return "tak"
-            }
+function gra(ruchGracza, ruchKomputera){
+    console.log("gracz: " + ruchGracza);
+    console.log("komputer: " + ruchKomputera);
+    if(ruchGracza == "FIRE"){
+        if(ruchKomputera == "WATER"){
+            return "nie";
         }
-        if(RuchGracz == "NOŻYCE"){
-            if(RuchKomputer == "KAMIEŃ"){
-                return "nie"
-            }
-            else if(RuchKomputer == "PAPIER"){
-                return "tak"
-            }
+        else if(ruchKomputera == "GRASS"){
+            return "tak";
         }
     }
-
-    
-    let scoreGracz = 0
-    let scoreKomputer = 0
-    let mecz
-
-while(scoreGracz < rundy || scoreKomputer < rundy){
-    mecz = gra(GraczRuch(), KomputerRuch())
-        if(mecz == "tak"){
-            scoreGracz += 1
-        console.log("KOMPUTER vs GRACZ\n    "+scoreKomputer+"         "+scoreGracz)
+    if(ruchGracza == "WATER"){
+        if(ruchKomputera == "GRASS"){
+            return "nie";
         }
-        else if(mecz == "nie"){
-        scoreKomputer += 1
-        console.log("KOMPUTER vs GRACZ\n    "+scoreKomputer+"         "+scoreGracz)
+        else if(ruchKomputera == "FIRE"){
+            return "tak";
         }
-        else{
-            console.log("KOMPUTER vs GRACZ\n    "+scoreKomputer+"         "+scoreGracz)
-        }
-    if(scoreGracz == rundy){
-        alert("gracz wygrał")
-        break;
     }
-    else if(scoreKomputer == rundy){
-        alert("Komputer wygrał")
-        break;
+    if(ruchGracza == "GRASS"){
+        if(ruchKomputera == "FIRE"){
+            return "nie";
+        }
+        else if(ruchKomputera == "WATER"){
+            return "tak";
+        }
     }
 }
+
+    function RundeWygrał(RuchGracza){
+    if(scoreKomputer == maxScore || scoreGracz == maxScore){
+        return
+    }
+    else{
+    let rundeWygrałGracz = gra(RuchGracza, KomputerRuch());
+    if(rundeWygrałGracz == "tak"){
+        scoreGracz += 1;
+        Gracz.textContent = scoreGracz;
+        Komputer.textContent = scoreKomputer;
+    }
+    else if(rundeWygrałGracz == "nie"){
+        scoreKomputer += 1;
+        Gracz.textContent = scoreGracz;
+        Komputer.textContent = scoreKomputer;
+    }
+    else{
+        Gracz.textContent = scoreGracz;
+        Komputer.textContent = scoreKomputer;
+    }
+    if(scoreGracz == maxScore){
+        Wygrał.textContent = "GRACZ WYGRAŁ"
+        Body.appendChild(Wygrał)
+        return;
+    }
+    else if(scoreKomputer == maxScore){
+        Wygrał.textContent = "KOMPUTER WYGRAŁ"
+        Body.appendChild(Wygrał)
+        return;
+    }
+    }
+}
+Fire.addEventListener("click", () => {
+    let ruchGracza = "FIRE";
+    RundeWygrał(ruchGracza);
+});
+
+Water.addEventListener("click", () => {
+    let ruchGracza = "WATER";
+    RundeWygrał(ruchGracza);
+});
+
+Grass.addEventListener("click", () => {
+    let ruchGracza = "GRASS";
+    RundeWygrał(ruchGracza);
+});
+
